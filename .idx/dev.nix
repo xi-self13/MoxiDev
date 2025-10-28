f7bfd08837878f2df8463db9960daafc3d82d599
ronreacusadams12 <ronreacusadams12@gmail.com>
2025-10-28 13:28:52 +0000

    Added gitignore

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,11 +5,7 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    
   ];
   # Sets environment variables in the workspace
   env = {};
