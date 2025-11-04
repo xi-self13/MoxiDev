233f34680f05ae7163b508576755c2ccd59d5b70
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2025-11-04 16:14:06 +0000

    Main menu visuals - improved.

---
+++ UI/scripts/button_panel_menu_main.gd
@@ -0,0 +1,16 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_settings_pressed() -> void:
+	var SettingsWindows : Window = %SettingsWindow
+	SettingsWindows.visible = true
